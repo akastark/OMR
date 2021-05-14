5ae7de1f9270c802e2990ef94650c722308641ef
Felipe Stark <felipeaugustostark@gmail.com>
2021-05-14 15:07:20 -0300

    Função com pré processamento da imagem de questões

---
+++ preprocessamento.py
@@ -0,0 +1,24 @@
+
+
+#########################################################
+#  ARQUIVO COM O CODIGO DE PRE PROCESSAMENTO DA IMAGEM  #
+#########################################################
+
+import cv2
+import numpy as np
+
+
+def PreProcessamento(imagem, largura, altura):
+    # redimensiona a imagem
+    imagem = cv2.resize(imagem, (largura, altura))
+
+    # tranforma a imagem em escala de cinza
+    img_gray = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+
+    # aplica gaussian blur
+    img_blur = cv2.GaussianBlur(img_gray, (5, 5), 1)
+
+    # detecao de contorno com algoritmo de Canny
+    img_canny = cv2.Canny(img_blur, 10, 50)
+
+    return img_canny
