5a3a4bbecedab9715d5cf8cceda2db3f355b9914
Felipe Stark <felipeaugustostark@gmail.com>
2021-05-17 15:24:11 -0300

    Refatoração de pastas

---
+++ codigo/preprocessamento.py
@@ -0,0 +1,22 @@
+
+
+#########################################################
+#  ARQUIVO COM O CODIGO DE PRE PROCESSAMENTO DA IMAGEM  #
+#########################################################
+
+import cv2
+import numpy as np
+
+
+def preProcessamento(imagem):
+
+    # tranforma a imagem em escala de cinza
+    img_gray = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+
+    # aplica gaussian blur para reduzir ruido
+    img_blur = cv2.GaussianBlur(img_gray, (5, 5), 1)
+
+    # detecao de contorno com algoritmo de Canny
+    img_canny = cv2.Canny(img_blur, 10, 50)
+
+    return img_canny
